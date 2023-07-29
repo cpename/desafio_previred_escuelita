25b789c342941378b21aa57ed91ed60c10d447b2
Cristian Pena <cristianpenamesina@gmail.com>
2023-07-29 18:40:14 -0400

    commit inicial

---
+++ src/main/java/com/cpena/previred/backend/apirest/PreviredBackendApirestApplication.java
@@ -0,0 +1,13 @@
+package com.cpena.previred.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreviredBackendApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreviredBackendApirestApplication.class, args);
+	}
+
+}
